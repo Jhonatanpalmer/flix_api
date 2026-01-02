9e2c94b0595da53721d3fc42ab1b7e0ba9e217f7
Jhonatan Palmer <jhonatanpalmer@hotmail.com>
2026-01-02 10:31:45 -0300

    Normalize line endings

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,8 +1,40 @@
+
+#### Importar o AVG para calcular a média das avaliações
+from django.db.models import Avg
+
+
 from rest_framework import serializers
 from movies.models import Movie
+from genres.models import Genre
+from actors.models import Actor
 
-class MovieSerializer(serializers.ModelSerializer):
+
+class MovieModelSerializer(serializers.ModelSerializer):
+    #### calcular a média das avaliações 
+    rate = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Movie
         fields = '__all__'
         
+    ### função para calcular a média das avaliações, vem do ORM do Django
+    def get_rate(self, obj):
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+        
+        if rate:
+            return rate
+        return None
+  
+        
+### validar datas antes de salvar ou atualizar
+    def validate_release_date(self, value):
+        if value.year < 1900:
+            raise serializers.ValidationError("A data nao pode ser anterior a 1900.")
+        return value
+
+#### Validar número de caracteres do resumo
+    def validate_resume(self, value):
+        if len(value) < 500:
+            raise serializers.ValidationError("O resumo deve ser maior que 200 caracteres.")
+        return value
+    
+    
